be6bdf5f0b4f2e2cff0d79b2fca6fb18a20e13e8
heidichen90 <toddypet@gmail.com>
2021-06-20 00:25:11 +0100

    edit index page css style and render category list, record list

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const exhbs = require("express-handlebars");
 const Record = require("./models/record");
+const Category = require("./models/category");
 require("./config/mongoose");
 
 const PORT = 3000;
@@ -11,11 +12,30 @@ app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  Record.find()
-    .lean()
-    .sort({ _id: "asc" })
-    .then((records) => res.render("index", { records }))
-    .catch((err) => console.log(err));
+  const recordPromise = Record.find().lean().sort({ _id: "asc" });
+  const categoryPromise = Category.find().lean().sort({ _id: "asc" });
+  Promise.all([recordPromise, categoryPromise])
+    .then((value) => {
+      const records = value[0];
+      const category = value[1];
+      let totalAmount = 0;
+
+      // format categories object for easy lookup
+      const categoryToClass = new Object();
+      category.forEach((e) => (categoryToClass[e.name] = e.icon_class));
+
+      // format record object
+      records.forEach((e) => {
+        e.iconClass = categoryToClass[e.category];
+        e.date = e.date.toDateString();
+        totalAmount += e.amount;
+      });
+
+      res.render("index", { records, category, totalAmount });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // activate and set port 3000
