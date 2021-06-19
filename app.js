68bc70dd0b062db2d426e8a1a39891d8b23477fb
heidichen90 <toddypet@gmail.com>
2021-06-19 01:18:42 +0100

    set up handlebars, main.hbs, index.hbs

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const exhbs = require("express-handlebars");
 require("./config/mongoose");
 
 const PORT = 3000;
 
+//set handlebars as a view engine
+app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-  res.send(`This is my expense app`);
+  res.render("index");
 });
 
 // activate and set port 3000
