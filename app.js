b4d550610e4dd5781d1282382c8fe664dd2fe2b6
heidichen90 <toddypet@gmail.com>
2021-06-19 14:28:30 +0100

    load seeder data to index page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const exhbs = require("express-handlebars");
+const Record = require("./models/record");
 require("./config/mongoose");
 
 const PORT = 3000;
@@ -10,7 +11,11 @@ app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  Record.find()
+    .lean()
+    .sort({ _id: "asc" })
+    .then((records) => res.render("index", { records }))
+    .catch((err) => console.log(err));
 });
 
 // activate and set port 3000
