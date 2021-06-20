e67235031f1dcf8801f846d521e7f76a1f57cdd1
heidichen90 <toddypet@gmail.com>
2021-06-20 01:26:20 +0100

    add filter by category

--- app.js
+++ app.js
@@ -18,8 +18,38 @@ app.use(express.urlencoded({ extended: true }));
 //set method-override
 app.use(methodOverride("_method"));
 
+//home page
 app.get("/", (req, res) => {
   const recordPromise = Record.find().lean().sort({ _id: "asc" });
+  const categoryPromise = Category.find().lean().sort({ _id: "asc" });
+  return Promise.all([recordPromise, categoryPromise])
+    .then((value) => {
+      const records = value[0];
+      const category = value[1];
+      let totalAmount = 0;
+
+      // format categories object for easy lookup
+      const categoryToClass = new Object();
+      category.forEach((e) => (categoryToClass[e.name] = e.icon_class));
+
+      // format record object
+      records.forEach((e) => {
+        e.iconClass = categoryToClass[e.category];
+        e.date = e.date.toDateString();
+        totalAmount += e.amount;
+      });
+
+      res.render("index", { records, category, totalAmount });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//filter a category
+app.get("/filter/:category", (req, res) => {
+  const categoryParam = req.params.category;
+  const recordPromise = Record.find().lean().find({ category: categoryParam });
   const categoryPromise = Category.find().lean().sort({ _id: "asc" });
   return Promise.all([recordPromise, categoryPromise])
     .then((value) => {
