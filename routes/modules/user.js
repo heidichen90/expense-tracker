d1972e84cf80ed9efb3bd1419ea76384ee414b56
heidichen90 <toddypet@gmail.com>
2021-10-27 22:28:56 +0100

    add login page handlebars

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -4,6 +4,10 @@ const User = require('../../models/user')
 
 const bcrypt = require('bcryptjs')
 
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
 router.get('/register', (req, res) => {
   res.render('register')
 })
