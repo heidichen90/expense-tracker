804dcb852f36278f5e2247a98ece0b3e785f6bd4
heidichen90 <toddypet@gmail.com>
2021-10-28 22:30:31 +0100

    fix styling

--- config/passport.js
+++ config/passport.js
@@ -1,54 +1,54 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
-const User = require("../models/user");
-const bcrypt = require("bcryptjs");
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = (app) => {
   // passport setup
-  app.use(passport.initialize());
-  app.use(passport.session());
+  app.use(passport.initialize())
+  app.use(passport.session())
 
   passport.use(
     // Local Strategy
-    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
       User.findOne({ email })
         .then((user) => {
           if (!user) {
             return done(null, false, {
-              message: "This email is not registered.",
-            });
+              message: 'This email is not registered.'
+            })
           }
           return bcrypt.compare(password, user.password).then((isMatch) => {
             if (!isMatch) {
               return done(null, false, {
-                message: "Email or Password incorrect.",
-              });
+                message: 'Email or Password incorrect.'
+              })
             }
-            return done(null, user);
-          });
+            return done(null, user)
+          })
         })
-        .catch((err) => done(err, false));
+        .catch((err) => done(err, false))
     })
-  );
+  )
 
-  //facebook strategy
+  // facebook strategy
   passport.use(
     new FacebookStrategy(
       {
         clientID: process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
         callbackURL: process.env.FACEBOOK_CALLBACK,
-        profileFields: ["email", "displayName"],
+        profileFields: ['email', 'displayName']
       },
       (accessToken, refreshToken, profile, done) => {
-        const { email, name } = profile._json;
+        const { email, name } = profile._json
         User.findOne({ email }).then((user) => {
           if (user) {
-            return done(null, user);
+            return done(null, user)
           }
 
-          const randomPassword = Math.random().toString(36).slice(-8);
+          const randomPassword = Math.random().toString(36).slice(-8)
           bcrypt
             .genSalt(10)
             .then((salt) => bcrypt.hash(randomPassword, salt))
@@ -56,24 +56,24 @@ module.exports = (app) => {
               User.create({
                 name,
                 email,
-                password: hash,
-              });
+                password: hash
+              })
             })
             .then((user) => done(null, user))
-            .catch((err) => done(err, false));
-        });
+            .catch((err) => done(err, false))
+        })
       }
     )
-  );
+  )
 
   // Session
   passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+    done(null, user.id)
+  })
   passport.deserializeUser((id, done) => {
     User.findById(id)
       .lean()
       .then((user) => done(null, user))
-      .catch((err) => done(err, null));
-  });
-};
+      .catch((err) => done(err, null))
+  })
+}
