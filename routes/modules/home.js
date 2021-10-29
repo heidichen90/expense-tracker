d47ca3182faed292b786d1234bf8c4fa262a58bd
heidichen90 <toddypet@gmail.com>
2021-10-29 19:43:35 +0100

    set up procfile

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,67 +1,67 @@
-const express = require("express");
-const router = express.Router();
-const Record = require("../../models/record");
-const Category = require("../../models/category");
-const { getTotalAmount } = require("../../public/javascript/tools");
+const express = require('express')
+const router = express.Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+const { getTotalAmount } = require('../../public/javascript/tools')
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const userId = req.user._id;
+    const userId = req.user._id
     const records = await Record.find({ userId })
-      .populate("categoryId")
+      .populate('categoryId')
       .lean()
-      .sort({ _id: "asc" });
-    const categoryList = await Category.find().lean().sort({ _id: "asc" });
+      .sort({ _id: 'asc' })
+    const categoryList = await Category.find().lean().sort({ _id: 'asc' })
 
     categoryList.forEach(
       (category) =>
         (category.name =
           category.name[0].toUpperCase() + category.name.slice(1))
-    );
+    )
 
     records.forEach((e) => {
-      e.iconClass = e.categoryId.icon_class;
-    });
+      e.iconClass = e.categoryId.icon_class
+    })
 
-    const totalAmount = getTotalAmount(records);
+    const totalAmount = getTotalAmount(records)
 
-    res.render("index", {
+    res.render('index', {
       records,
       category: categoryList,
-      totalAmount,
-    });
+      totalAmount
+    })
   } catch (error) {
-    console.log(error);
-    res.render("error", { error });
+    console.log(error)
+    res.render('error', { error })
   }
-});
+})
 
 // filter a category
-router.get("/filter/:category", async (req, res) => {
-  const userId = req.user._id;
+router.get('/filter/:category', async (req, res) => {
+  const userId = req.user._id
   try {
-    const categoryParam = req.params.category;
+    const categoryParam = req.params.category
     const records = await Record.find({ category: categoryParam, userId })
-      .populate("categoryId")
-      .lean();
-    const categoryList = await Category.find().lean().sort({ _id: "asc" });
+      .populate('categoryId')
+      .lean()
+    const categoryList = await Category.find().lean().sort({ _id: 'asc' })
 
     records.forEach((e) => {
-      e.iconClass = e.categoryId.icon_class;
-    });
+      e.iconClass = e.categoryId.icon_class
+    })
 
-    const totalAmount = getTotalAmount(records);
+    const totalAmount = getTotalAmount(records)
 
-    res.render("index", {
+    res.render('index', {
       records,
       category: categoryList,
       totalAmount,
-      selectedCategory: categoryParam,
-    });
+      selectedCategory: categoryParam
+    })
   } catch (error) {
-    console.log(error);
-    res.render("error", { error });
+    console.log(error)
+    res.render('error', { error })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,76 +1,76 @@
-const express = require("express");
-const router = express.Router();
-const Record = require("../../models/record");
-const Category = require("../../models/category");
-const categoryData = require("../../mock_data/category.json");
+const express = require('express')
+const router = express.Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+const categoryData = require('../../mock_data/category.json')
 
 // delete a record
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
   return Record.findById(id)
     .then((record) => {
-      return record.remove();
+      return record.remove()
     })
     .then(() => {
-      res.redirect("/");
+      res.redirect('/')
     })
     .catch((error) => {
-      console.log(error);
-      res.render("error", { error });
-    });
-});
+      console.log(error)
+      res.render('error', { error })
+    })
+})
 
 // edit a record
-router.get("/:id/edit", (req, res) => {
-  const id = req.params.id;
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
   return Record.findById(id)
     .lean()
     .then((record) => {
       // convert it to unit timestamp
-      res.render("edit", { record, category: categoryData.categorySeeds });
+      res.render('edit', { record, category: categoryData.categorySeeds })
     })
     .catch((error) => {
-      console.log(error);
-      res.render("error", { error });
-    });
-});
+      console.log(error)
+      res.render('error', { error })
+    })
+})
 
 // put to update record
-router.put("/:id", (req, res) => {
-  const id = req.params.id;
+router.put('/:id', (req, res) => {
+  const id = req.params.id
   return Record.findById(id)
     .then((record) => {
-      Object.assign(record, { ...req.body });
-      return record.save();
+      Object.assign(record, { ...req.body })
+      return record.save()
     })
     .then(() => {
-      res.redirect("/");
+      res.redirect('/')
     })
     .catch((error) => {
-      res.render("error", { error });
-    });
-});
+      res.render('error', { error })
+    })
+})
 
 // add new record
-router.get("/new", (req, res) => {
-  const today = new Date();
-  res.render("new", { today, category: categoryData.categorySeeds });
-});
+router.get('/new', (req, res) => {
+  const today = new Date()
+  res.render('new', { today, category: categoryData.categorySeeds })
+})
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const userId = req.user._id;
-    const categoryId = await Category.findOne({ name: req.body.category });
-    const expense = new Record({ ...req.body, userId, categoryId });
+    const userId = req.user._id
+    const categoryId = await Category.findOne({ name: req.body.category })
+    const expense = new Record({ ...req.body, userId, categoryId })
 
-    const saveNewRecord = await expense.save();
+    const saveNewRecord = await expense.save()
 
     if (saveNewRecord) {
-      res.redirect("/");
+      res.redirect('/')
     }
   } catch (error) {
-    res.render("error", { error });
+    res.render('error', { error })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
